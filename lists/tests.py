c34ae6ebdf48b3af8a3a5297f6efd3850aab6463
Hazzari <Freee2mmm@gmail.com>
2020-08-28 23:14:14 +0300

    added first module test adn resolve url, dummy view.

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    '''Тест домашней страницы'''
+
+    def test_root_urls_resolves_to_home_page_view(self):
+        '''тест: / url преобразуется в view домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
