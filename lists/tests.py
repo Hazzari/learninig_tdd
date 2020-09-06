8f248a5f0345c679d58aa82c90d73407ed99b3a2
Hazzari <dev.aleksan@gmail.com>
2020-09-07 01:03:22 +0300

    feat: add test home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     '''Тест домашней страницы'''
@@ -9,3 +11,15 @@ class HomePageTest(TestCase):
         '''тест: / url преобразуется в view домашней страницы'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''тест: Домашняя страница возвращает правильный html'''
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        html = response.content.decode('utf-8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
