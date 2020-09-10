488b6f95401750742042973e0d2bdbc0471a881f
Hazzari <dev.aleksan@gmail.com>
2020-09-10 10:53:58 +0300

    style(tests): convert quote single to dual

--- lists/tests.py
+++ lists/tests.py
@@ -5,15 +5,15 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    '''Тест домашней страницы'''
+    """Тест домашней страницы"""
 
     def test_root_urls_resolves_to_home_page_view(self):
-        '''тест: / url преобразуется в view домашней страницы'''
+        """тест: / url преобразуется в view домашней страницы"""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        '''тест: Домашняя страница возвращает правильный html'''
+        """тест: Домашняя страница возвращает правильный html"""
 
         request = HttpRequest()
         response = home_page(request)
