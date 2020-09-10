521c10881acfc2eefc36318c546dd7f15b0ce87a
Hazzari <dev.aleksan@gmail.com>
2020-09-10 11:21:26 +0300

    refactor: Refactored views index page to use templated

--- lists/tests.py
+++ lists/tests.py
@@ -7,19 +7,25 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
-    def test_root_urls_resolves_to_home_page_view(self):
-        """тест: / url преобразуется в view домашней страницы"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_used_index_template(self):
+        """Используется index шаблон"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/index.html')
 
-    def test_home_page_returns_correct_html(self):
-        """тест: Домашняя страница возвращает правильный html"""
+    # def test_root_urls_resolves_to_home_page_view(self):
+    #     """тест: / url преобразуется в view домашней страницы"""
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
+    #
+    # def test_home_page_returns_correct_html(self):
+    #     """тест: Домашняя страница возвращает правильный html"""
+    #
+    #     response = self.client.get('/')
+    #
+    #     html = response.content.decode('utf-8')
+    #
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.endswith('</html>'))
 
-        request = HttpRequest()
-        response = home_page(request)
 
-        html = response.content.decode('utf-8')
-
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
