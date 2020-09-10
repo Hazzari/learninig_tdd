bcfd1d17a0f43910276c4d131368e0f2273e61dc
Hazzari <dev.aleksan@gmail.com>
2020-09-10 22:44:18 +0200

    test(FT): extend multiuser lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,8 +6,11 @@ import time
 import unittest
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 3
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -16,11 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
 
-    def check_for_row_in_table(self, row_text):
+    def wait_for_row_in_lists_table(self, row_text):
         """подтверждение строки в таблице списка"""
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def tearDown(self) -> None:
         self.browser.quit()
@@ -46,25 +57,67 @@ class NewVisitorTest(LiveServerTestCase):
         # Пользователь нажимает enter, страница обновляется, и теперь страница содержит:
         # "1: Сходить в магазин за продуктами" в качестве элемента списка
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_table('1: Сходить в магазин за продуктами')
+        self.wait_for_row_in_lists_table('1: Сходить в магазин за продуктами')
 
         # Текстовое поле по прежнему приглашает пользователя добавить еще один элемент.
         # Пользователь вводит "Купить молоко и хлеб" и нажимает enter
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Купить молоко и хлеб')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # Страница обновляется, и теперь показывает оба элемента списка
-        self.check_for_row_in_table('1: Сходить в магазин за продуктами')
-        self.check_for_row_in_table('2: Купить молоко и хлеб')
+        self.wait_for_row_in_lists_table('1: Сходить в магазин за продуктами')
+        self.wait_for_row_in_lists_table('2: Купить молоко и хлеб')
 
         # Пользователь думает, запомнит ли сайт его список и видит что сайт сгенерировал для него уникальный URL-адрес
         # об этом выводится небольшой текст с обяснениями.
         # Пользователь посещает этот URL и видит что он отображает правильно его список.
         # Пользователь заканчивает работу с сайтом
         # ------- >>>>> Конец сценария
+
+        self.fail('Написать следующий тест')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        """тест: многочисленные пользователи могут создавать списки по разным url"""
+        # Первый пользователь создает новый список
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Сходить в магазин за продуктами')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_lists_table('1: Сходить в магазин за продуктами')
+
+        # пользователь видит что его список имеет уникальный url
+        first_user_list_url = self.browser.current_url
+        self.assertRegex(first_user_list_url, '/lists/.+')
+
+        # Теперь приходит новый пользователь на сайт.
+
+        # ##  Мы используем новый сеанс браузера для обеспечения изолированности от первого пользователя. cookie и др.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Второй пользователь посещяет домашнюю страницу.Нет никаких признаков списка первого пользователя
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Сходить в магазин за продуктами', page_text)
+        self.assertNotIn('Купить молоко и хлеб', page_text)
+
+        # Второй пользователь начинает новый список, вводя новый элемент.
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Купить молоко')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_lists_table('1: Купить молоко')
+
+        # второй пользователь получает уникальный URL адрес
+        second_user_list_url = self.browser.current_url
+        self.assertRegex(second_user_list_url, 'lists/.+')
+        self.assertNotEqual(second_user_list_url, first_user_list_url)
+
+        # Нет никаокго следа от списка первого пользователя
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('ходить в магазин за продуктами', page_text)
+        self.assertIn('Купить молоко', page_text)
 
         self.fail('Написать следующий тест')
