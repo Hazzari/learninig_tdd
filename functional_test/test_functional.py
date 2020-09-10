502aaabe5f83c11f30052fb951a074da3e4179bf
Hazzari <dev.aleksan@gmail.com>
2020-09-10 10:52:56 +0300

    test(FT): FT now checks the ability to enter a list item.

--- functional_test/test_functional.py
+++ functional_test/test_functional.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Функциональный тест действий пользователя на сайте
-"""
+"""Функциональный тест действий пользователя на сайте."""
+import time
 
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -27,11 +28,24 @@ class NewVisitorTest(unittest.TestCase):
         # проверяем правильная ли у нас страница:
         # >>> Заголовок и шапка страницы указывают что это список дел
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Пользователю предлагается ввести элемент списка
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
         # Пользователь набирает в текстовом поле "Сходить в магазин за продуктами"
+        input_box.send_keys('Сходить в магазин за продуктами')
         # Пользователь нажимает enter, страница обновляется, и теперь страница содержит:
         # "1: Сходить в магазин за продуктами" в качестве элемента списка
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == "1: Сходить в магазин за продуктами" for row in rows))
+
         # Текстовое поле по прежнему приглашает пользователя добавить еще один элемент.
         # Пользователь вводит "Купить молоко и хлеб" и нажимает enter
         # Страница обновляется, и теперь показывает оба элемента списка
