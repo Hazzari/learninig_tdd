adcadcce14e2f67024c1c31e8873c52cc7febeab
Hazzari <dev.aleksan@gmail.com>
2020-09-26 18:45:11 +0300

    refactor: add urls list/new, move test to new class, remove view home_page - if and create new view new_list

--- learninig_tdd/urls.py
+++ learninig_tdd/urls.py
@@ -20,5 +20,6 @@ from lists import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home'),
+    path('lists/new', views.new_list, name='new_list'),
     path('lists/unique-of-a-kind-list/', views.view_list, name='view_list'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -5,16 +5,16 @@ from lists.models import Item
 
 def home_page(request):
     """Домашняя страница"""
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/unique-of-a-kind-list')
     return render(request, 'index.html')
 
 
 def view_list(request):
-
+    """Новый список"""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/unique-of-a-kind-list/')
 
